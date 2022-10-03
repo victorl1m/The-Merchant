edc57c3e6fc0c092356589750ad796d63b997476
trackedby <trackedby1@gmail.com>
2022-10-03 16:33:29 -0300

    new navbar

--- src/Wrapper.jsx
+++ src/Wrapper.jsx
@@ -2,6 +2,7 @@ import "./assets/global.css";
 import TopBar from "./components/TopBar/TopBar.jsx";
 import Home from "./components/Home/Home.jsx";
 import PubInfo from "./components/PubInfo/PubInfo";
+import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Article from "./components/Article/Article.jsx";
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/article/:id" element={<Article />} />
           <Route path="/pubinfo/:id" element={<PubInfo />} />
         </Routes>
+        <Navbar />
       </BrowserRouter>
     </>
   );
